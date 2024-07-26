a407baa6846f4cf897daddbb9ba3fb1ed67a1ffc
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-07-26 10:02:59 +0700

    update: webhook

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, ClerkMiddlewareOptions } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: ["/api/webhooks/clerk"],
+} as ClerkMiddlewareOptions);
 
 export const config = {
   matcher: [
