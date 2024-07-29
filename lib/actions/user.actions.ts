d9aaabc5bec382aecb642831ad759581f3cfd794
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-07-29 09:46:12 +0700

    fix: error 500 server

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -78,6 +78,8 @@ export async function updateCredits(userId: string, creditFee: number) {
   try {
     await connectToDatabase();
 
+    console.log(userId);
+
     const updatedUserCredits = await User.findOneAndUpdate(
       { _id: userId },
       { $inc: { creditBalance: creditFee }},
