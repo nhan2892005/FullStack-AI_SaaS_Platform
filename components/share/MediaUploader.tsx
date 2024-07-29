8a68f82c0fcbfa448ad2fd69d8eff244ee803fe1
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-07-29 15:45:38 +0700

    fix

--- components/share/MediaUploader.tsx
+++ components/share/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="nhannguyen"
+      uploadPreset="phucnhan"
       options={{
         multiple: false,
         resourceType: "image",
