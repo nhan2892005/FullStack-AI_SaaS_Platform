c5ee76cebb3d3bdd5fd5af2083be4a6a6838598d
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-08-13 22:56:32 +0700

    update analyst for web

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import SideBar from '@/components/share/SideBar'
 import MobileNav from '@/components/share/MobileNav'
 import React from 'react'
 import { Toaster } from '@/components/ui/toaster'
+import { Analytics } from '@vercel/analytics/react';
 
 const Layout = ({ children }:{ children: React.ReactNode }) => {
   return (
@@ -11,6 +12,7 @@ const Layout = ({ children }:{ children: React.ReactNode }) => {
         <div className='root-container'>
             <div className='wrapper'>
                 {children}
+                <Analytics />
             </div>
         </div>
 
